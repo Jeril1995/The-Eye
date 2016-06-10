e26c8238fdc8f1a4cf7e925f7bc13f9ef4ccede3
Jeril1995 <jeril95.n@gmail.com>
2016-06-10 08:39:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/jeril/myapplication/SimpleAndroidOCRActivity.java
@@ -0,0 +1,286 @@
+package com.example.jeril.myapplication;
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Locale;
+import java.util.zip.GZIPInputStream;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.AudioManager;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class SimpleAndroidOCRActivity extends Activity {
+
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
+    public static final String lang = "eng";
+    private static final String TAG = "SimpleAndroidOCR.java";
+
+    protected EditText _field;
+    protected String _path;
+    protected boolean _taken;
+    String text;
+    TextToSpeech tts ;
+    int result;
+    protected static final String PHOTO_TAKEN = "photo_taken";
+
+    
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        tts = new TextToSpeech(SimpleAndroidOCRActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+
+                    result = tts.setLanguage(Locale.US);
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(),
+                            "Feature not supported in your device",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        String[] paths = new String[]{DATA_PATH, DATA_PATH + "tessdata/"};
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+
+                byte[] buf = new byte[1024];
+                int len;
+
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+
+                out.close();
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        _field = (EditText) findViewById(R.id.field);
+        _path = DATA_PATH + "/ocr.jpg";
+      }
+
+        public void speak(int h) {
+
+
+                if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
+
+                    Toast.makeText(getApplicationContext(),
+                            "Feature not supported in your device",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+
+                    text = _field.getText().toString();
+                    if(h==1)
+                     tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+                    else if(h==0)
+                        tts.speak("camera app opened",TextToSpeech.QUEUE_FLUSH, null);
+                    else if (h==2)
+                        tts.speak("texts extracted",TextToSpeech.QUEUE_FLUSH, null);
+
+
+
+                }
+            }
+
+
+
+
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent e) {
+        switch(keycode) {
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                speak(0);
+                new ButtonClickHandler();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                speak(1);
+                return true;
+
+
+        }
+
+        return super.onKeyDown(keycode, e);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+    }
+    public class ButtonClickHandler  {
+         {
+            _field.setText("");
+            Log.v(TAG, "Starting Camera app");
+            startCameraActivity();
+        }
+    }
+    protected void startCameraActivity() {
+
+        File file = new File(_path);
+        Uri outputFileUri = Uri.fromFile(file);
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(intent, 0);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "resultCode: " + resultCode);
+
+        if (resultCode == -1) {
+            onPhotoTaken();
+        } else {
+            Log.v(TAG, "User cancelled");
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN, _taken);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(TAG, "onRestoreInstanceState()");
+        if (savedInstanceState.getBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN)) {
+            onPhotoTaken();
+        }
+    }
+    protected void onPhotoTaken() {
+        _taken = true;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(_path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        Log.v(TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(DATA_PATH, lang);
+        baseApi.setImage(bitmap);
+
+        String recognizedText = baseApi.getUTF8Text();
+
+        baseApi.end();
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9&,/-_*%#@$!()}{]+", " ");
+        }
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0) {
+            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            _field.setSelection(_field.getText().toString().length());
+            speak(2);
+        }
+    }
+
+}
